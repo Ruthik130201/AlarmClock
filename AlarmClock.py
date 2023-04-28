eb84242cfe41742dadb1aeb9bbec43fd2f7b379e
Ruthik130201 <108323632+Ruthik130201@users.noreply.github.com>
2023-04-28 18:37:38 +0530

    Add files via upload

---
+++ AlarmClock.py
@@ -0,0 +1,40 @@
+from tkinter import *
+import datetime
+import time
+import winsound
+
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime("%d/%m/%Y")
+        print("The Set Date is:",date)
+        print(now)
+        if now == set_alarm_timer:
+            print("Time to Wake up")
+            winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
+            break
+
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
+
+clock = Tk()
+clock.title("Wake Up It's Time")
+clock.geometry("350x150")
+time_format=Label(clock, text= "24 hour format!", fg="purple",bg="Green",font="Arial").place(x=100,y=120)
+addTime = Label(clock,text = "Hr's  Min's   Sec's",font=60).place(x = 110)
+setYourAlarm = Label(clock,text = "Set Alarm",fg="purple",relief = "solid",font=("Helevetica",8,"bold")).place(x=25, y=29)
+
+hour = StringVar()
+min = StringVar()
+sec = StringVar()
+
+hourTime= Entry(clock,textvariable = hour,bg = "lightblue",width = 15).place(x=110,y=30)
+minTime= Entry(clock,textvariable = min,bg = "lightblue",width = 15).place(x=150,y=30)
+secTime = Entry(clock,textvariable = sec,bg = "lightblue",width = 15).place(x=200,y=30)
+
+submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =130,y=70)
+
+clock.mainloop()
